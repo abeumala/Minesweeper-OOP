87ac87e0726d88877f74bf785ff79b1e7a4821be
Arnau Beumala Garcia <arnaubeumalagarcia@MacBook-Air-de-Arnau.local>
2019-04-15 20:49:54 +0200

    numbers and flag implementation

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 const field = document.getElementById('field');
 const cell = document.getElementsByClassName('cell');
 console.log(cell, 'cell');
-const cols = 10;
-const rows = 10;
-
+const cols = 20;
+const rows = 15;
 
 function createField(cols, rows) {
+
 	while (field.firstChild) {
     field.removeChild(field.firstChild);
 	}
@@ -29,21 +29,20 @@ function createField(cols, rows) {
 
 	for (let i = 0; i < rows; i++){
 		const row = document.createElement('div');
-		row.setAttribute('class', 'row hidden');
+		row.setAttribute('class', 'row');
 		for (let j = 0; j < cols; j++) {
 			const col = document.createElement('div')
 			col.setAttribute('class', 'col hidden cell');
+			col.setAttribute('id', `${i} ${j}`);
 			if(coordenadas.includes(`[${i},${j}]`)){
 				col.setAttribute('class', 'col bomb cell')
 				row.appendChild(col);
 			}
 			row.appendChild(col);
-
 		}
 		field.appendChild(row);
 	}
-	
-	makeMyCell()
+	makeCellClickable()
 }
 
 createField(cols,rows);
@@ -63,74 +62,94 @@ function gameOver (clickInput) {
 	
 }
 
-function makeMyCell () {
+function makeCellClickable () {
 	for (var i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', function () {
+		cell[i].addEventListener('contextmenu', function(ev) {
+		    ev.preventDefault();
+		    let myCell = this;
+		    if (myCell.children[0] && myCell.children[0].nodeName == 'IMG') {
+		    	myCell.innerHTML = ""
+		    }else{
+				myCell.innerHTML = "<img class='bombImage' src='flag.png'/>"
+		    }
+		    
+		    return false;
+		}, false);
+    	cell[i].addEventListener('click', function () {
     	const myCell = this;
     	if(myCell.classList.contains("bomb")) {
     		gameOver(true);
+    	 } else {
+    	 	let bombCount = 0;
+    	 	let id = myCell.id.split(" ");
+    	 	let y = parseInt(id[0]);
+    	 	let x = parseInt(id[1]);
+
+    	 	let upperCellPos = y - 1 + " " + x;
+    	 	let upperCell = document.getElementById(upperCellPos);
+
+    	 	bombCount += checkBomb(upperCell)
+
+    	 	let upperRightCellPos = y - 1 + " " + (x + 1);
+    	 	let upperRightCell = document.getElementById(upperRightCellPos);
+
+    	 	bombCount += checkBomb(upperRightCell)
+
+    	 	let upperLeftCellPos = y - 1 + " " + (x - 1);
+    	 	let upperLeftCell = document.getElementById(upperLeftCellPos);
+
+    	 	bombCount += checkBomb(upperLeftCell)
+
+    	 	let rightCellPos = y + " " + (x + 1);
+    	 	let rightCell = document.getElementById(rightCellPos);
+
+    	 	bombCount += checkBomb(rightCell)
+
+    	 	let leftCellPos = y + " " + (x - 1);
+    	 	let leftCell = document.getElementById(leftCellPos);
+
+    	 	bombCount += checkBomb(leftCell)
+
+    	 	let lowerRightCellPos = y + 1 + " " + (x + 1);
+    	 	let lowerRightCell = document.getElementById(lowerRightCellPos);
+
+    	 	bombCount += checkBomb(lowerRightCell)
+
+    	 	let lowerLeftCellPos = y + 1 + " " + (x - 1);
+    	 	let lowerLeftCell = document.getElementById(lowerLeftCellPos);
+
+    	 	bombCount += checkBomb(lowerLeftCell)
+
+    	 	let lowerCellPos = y + 1 + " " + x;
+    	 	let lowerCell = document.getElementById(lowerCellPos);
+
+    	 	bombCount += checkBomb(lowerCell)
+
+
+    	 	console.log(bombCount)
+
+    	 	if (bombCount != 0) myCell.innerHTML = "<span class='bombcount'>"+bombCount+"</span>";
+    	 	else myCell
     	}
   	});
   }	
 }
 
-
-function printNums () {
-	let counter = 0;
-	for (let i = -1; i = 1; i++) {
-		for (let j = -1; j = 1; j++){
-			if (!myCell.classList.contains("bomb")){
-
-			}
-		}
+function checkBomb(cell) {
+	if (cell && cell.classList.contains('bomb')) {
+		return 1
 	}
+	return 0
 }
 
 
-// cell.addEventListener("click", function(){
-// 	console.log(this);
-// });
-
-// function toggleClass () {
-
-// }
-
-
-// field.onclick = function(){
-// 	console.log(this);
-// }
-// function drawMines () {
-// 	for (let x = 0; x < field.length; x++) {
-// 	  for (let z = 0; z < field.length; z++) {
-//       	if (field[x][z] === field[rand1][rand2]) {
-// 	      	var elem = document.createElement('img'); //creates element by tagname
-// 					elem.src = "mineImg.png";
-// 					elem.setAttribute("class", "mine");
-// 		}	
-// 	}	
-// }
-
-
-
-
-
-
-// function createField(cols, rows) {
-// 	let counter = 0;
-// 	for (let i = 0; i < rows; i++){
-// 		const row = document.createElement('div');
-// 		row.setAttribute('class', 'row hidden');
-// 		for (let j = 0; j < cols; j++) {
-// 			const col = document.createElement('div')
-// 			col.setAttribute('class', 'col hidden');
-// 			// if (Math.random < 0.15)
-// 			if (counter < 20) {
-// 				col.setAttribute('class', 'bomb');
-// 				counter ++;
-// 			}
-// 			row.appendChild(col);
+// function CountBombs () {
+// 	bombCounter = 0;
+// 	for (var i = 0; i < rows; i++) {
+// 		for (var j = 0; j < cols; j++)
+// 			if ()
 // 		}
-// 		field.appendChild(row);
-// 	}
 
+// 	}
 // }
+
